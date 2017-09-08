1ad0f03caf7362aea33b91489ff5fb3cd33c0818
davidespano <davide.spano@unica.it>
2017-09-08 12:09:39 +0200

    lezione del 8/9/2017

---
+++ WMA-FPW17/src/java/model/User.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author davide
+ */
+public class User {
+    
+    public static final int ADMIN = 1;
+    public static final int REGISTERED = 0;
+    
+    private int id;
+    private String username;
+    private String password;
+    private int type;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the type
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+    
+}
---
+++ WMA-FPW17/src/java/model/UserFactory.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import servlet.SecondoAccessoDB;
+
+/**
+ *
+ * @author davide
+ */
+public class UserFactory {
+    
+    public UserFactory(){
+        // Inizializzo la libreria per la connessione al database
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SecondoAccessoDB.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public User getUserById(int id){
+        User toReturn = null;
+        
+        try {
+            // creazione e apertura della connessione
+            // si specifica la url, lo username e la password per il db
+            Connection conn = DriverManager.getConnection(
+                    Settings.DB_URL,
+                    Settings.DB_USR,
+                    Settings.DB_PSW
+            );
+            
+            // creo la query
+            String query = "select id, username, password, uType from users where id = ?";
+            
+            // creo l'istruzione sql
+            PreparedStatement stmt = conn.prepareStatement(query);
+            
+            // dico quale valore ci vada al posto del punto di domanda
+            // si specifica la posizione del punto di domanda nella stringa 
+            // della query iniziandoli a contare da 1 (noi ne abbiamo uno solo
+            // ma potrebbero essere anche di più) e si usa una set con il tipo 
+            // di dato corrisponente alla colonna del db (intero, stringa, booleano ecc.). Per noi 
+            // è un intero. 
+            stmt.setInt(1, id);
+            
+            ResultSet set = stmt.executeQuery();
+            
+            
+            if(set.next()){
+                // il corpo lo eseguo al massimo una volta
+                // in questo caso, poiché id è chiave primaria
+                
+                toReturn = new User();
+                
+                toReturn.setId(set.getInt("id"));
+                toReturn.setUsername(set.getString("username"));
+                toReturn.setPassword(set.getString("password"));
+                toReturn.setType(set.getInt("uType"));
+            }
+            
+            // Chiusura della connessione
+            conn.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UserFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return toReturn;
+    }
+    
+    public User getUserByUsernamePassword(String username, String password){
+        User toReturn = null;
+        
+        try {
+            // creazione e apertura della connessione
+            // si specifica la url, lo username e la password per il db
+            Connection conn = DriverManager.getConnection(
+                    Settings.DB_URL,
+                    Settings.DB_USR,
+                    Settings.DB_PSW
+            );
+            
+            // creo la query
+            String query = "select id, username, password, uType from users "
+                    + "where username = ? and password = ?";
+            
+            // creo l'istruzione sql
+            PreparedStatement stmt = conn.prepareStatement(query);
+            
+            
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            
+            ResultSet set = stmt.executeQuery();
+            
+            
+            if(set.next()){
+                // il corpo lo eseguo al massimo una volta
+                // in questo caso, poiché id è chiave primaria
+                
+                toReturn = new User();
+                
+                toReturn.setId(set.getInt("id"));
+                toReturn.setUsername(set.getString("username"));
+                toReturn.setPassword(set.getString("password"));
+                toReturn.setType(set.getInt("uType"));
+            }
+            
+            // Chiusura della connessione
+            conn.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UserFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return toReturn;
+    }
+    
+}
